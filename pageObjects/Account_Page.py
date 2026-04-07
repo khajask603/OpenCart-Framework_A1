bc34e7535a55876e707febdbe105cd411e0c8f6a
Shaik khaja Mohiddin <khajask603@gmail.com>
2026-04-07 22:43:37 +0530

    Code Maintenace-6 March 7th

--- pageObjects/Account_Page.py
+++ pageObjects/Account_Page.py
@@ -7,12 +7,14 @@ from pageObjects.base_page import BasePage
 
 class AccountPage(BasePage):
 
+
     def __init__(self, driver: WebDriver):
         # 1. Start the Base Engine (Syncs the Driver & Wait)
         super().__init__(driver)
 
     ACCOUNT_TEXT = (By.XPATH, "//span[contains(normalize-space(),'My Account')]")
     LOGOUT_BUTTON = (By.XPATH, "//ul[contains(@class,'dropdown-menu-right')]//li//a")
+
     def click_myAccountTxt(self):
         self.click_element(self.ACCOUNT_TEXT)
 
